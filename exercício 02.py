d887709fc4f40a4c4f2e576bd208fb2b996804e9
Murilo Mota <murilohfmota@gmail.com>
2024-04-09 14:21:38 -0300

    exercicio 02, condicionais e operadores logicos

---
+++ exercício 02.py
@@ -0,0 +1,24 @@
+
+dia =''
+
+
+dia = input('Que dia é hoje ?')
+
+if  dia == 'segunda' or dia == 'segunda-feira' or dia == 'segunda feira' :
+    print('Mais uma semana começando, vamos lá.')
+
+elif  dia == 'terça' or dia == 'terça-feira' or dia == 'terça feira' :
+    print( 'Pense pelo lado positivo, pelo menos não é segunda' )
+
+elif  dia == 'quarta' or dia == 'quarta-feira' or dia == 'quarta feira' :
+    print( 'Chegamos na metade, e nem foi tão ruim assim' )
+
+elif  dia == 'quinta' or dia == 'quinta-feira' or dia == 'quinta feira' :
+    print( 'Amanhã é sextaaa, aguenta mais um pouco!' )
+
+elif dia == 'sexta' or dia == 'sexta-feira' or dia == 'sexta feira':
+    print( 'HOJE É SEXTA FEIRAAA, LARGOU JA ERA, DIRETO PRA CASA DORMIR QUE TAS CANSADE' )
+
+
+    #  = == === , / % /= , < <= >= > 
+
