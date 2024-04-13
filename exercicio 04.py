1b696363bb439ac2d161fbf8eb786f42a527d51e
Murilo Mota <murilohfmota@gmail.com>
2024-04-13 19:45:53 -0300

    Finalização da versão 0.1 front end

--- exercicio 04.py
+++ exercicio 04.py
@@ -2,13 +2,14 @@ from tkinter import *
 
 root = Tk()
 class App():
+    #Definir telas e frames do front
     def __init__(self):
         self.root = root
         self.tela()
         self.frames_da_tela()
         self.Construtor_botões()
         self.Caixa_texto()
-
+        self.Widgets()
 
         root.mainloop()
         
@@ -30,32 +31,48 @@ class App():
     #construtor de botões
     def Construtor_botões(self):
         #botâo de executar
-        self.bt_executar = Button(self.frame_1, text = 'Executar')
+        self.bt_executar = Button(self.frame_1, text = 'Executar', bd = 2 , bg = '#184e77', fg = '#d9ed92', font = ('firacode', 12))
         self.bt_executar.place ( relx = 0.9, rely = 0.85, relwidth = 0.1, relheight = 0.15)
         #botão proximo
-        self.bt_proximo = Button(self.root, text = 'Proximo')
-        self.bt_proximo.place ( relx = 0.89, rely = 0.87, relwidth = 0.09, relheight = 0.08)
+        self.bt_proximo = Button(self.root, text = 'Proximo', bd = 2 , bg = '#52b69a', fg = '#184e77', font = ('firacode', 12))
+        self.bt_proximo.place ( relx = 0.89, rely = 0.87, relwidth = 0.1, relheight = 0.07)
         #botão sair
-        self.bt_proximo = Button(self.root, text = 'Sair')
-        self.bt_proximo.place ( relx = 0.02, rely = 0.87, relwidth = 0.09, relheight = 0.08)
+        self.bt_proximo = Button(self.root, text = 'Sair', bd = 2 , bg = '#99d98c', fg = '#184e77', font = ('firacode', 12))
+        self.bt_proximo.place ( relx = 0.02, rely = 0.87, relwidth = 0.1, relheight = 0.07)
     
     def Caixa_texto(self) :
         #texto de instrução da primeira tela
-        self.texto = Label(self.frame_1, text = "Informe dois números inteiros e um número real.", background = '#d9ed92')
+        self.texto = Label(self.frame_1, text = "Informe dois números inteiros e um número real.", background = '#d9ed92', fg = '#184e77', font = ('firacode', 12))
         self.texto.place ( relx = 0.0, rely = 0.010, relwidth = 0.3, relheight = 0.1 )
         #texto numero 1
-        self.texto01 = Label(self.frame_1, text = "Valor inteiro", background = '#d9ed92')
+        self.texto01 = Label(self.frame_1, text = "Primeiro inteiro", background = '#d9ed92', fg = '#184e77', font = ('firacode', 12))
         self.texto01.place ( relx = 0.3, rely = 0.010, relwidth = 0.3, relheight = 0.1 )
         #texto numero 2
-        self.texto02 = Label(self.frame_1, text = "Valor inteiro", background = '#d9ed92')
+        self.texto02 = Label(self.frame_1, text = "Segundo inteiro", background = '#d9ed92', fg = '#184e77', font = ('firacode', 12))
         self.texto02.place ( relx = 0.5, rely = 0.010, relwidth = 0.3, relheight = 0.1 )
         #texto numero 3
-        self.texto03 = Label(self.frame_1, text = "Valor real", background = '#d9ed92')
-        self.texto03.place ( relx = 0.7, rely = 0.010, relwidth = 0.3, relheight = 0.1 )
+        self.texto03 = Label(self.frame_1, text = "Valor real", background = '#d9ed92', fg = '#184e77', font = ('firacode', 12))
+        self.texto03.place ( relx = 0.7, rely = 0.010, relwidth = 0.3, relheight = 0.1,  )
 
+        #caixa de texto do resultado no frame_2
+        self.texto_resultado = Label(self.frame_2, text = "", background = '#d9ed92')
+        self.texto_resultado.place ( relx = 0.0, rely = 0.010, relwidth = 0.9, relheight = 0.9 )
+    def Widgets(self) : 
+        #criando campos de entrada de texto do usuario
+        #campo valor inteiro 01
+        self.entrada01 = Entry(self.frame_1)
+        self.entrada01.place ( relx = 0.40, rely = 0.10, relwidth = 0.1, relheight = 0.1 )
 
+        #campo valor inteiro 02
+        self.entrada02 = Entry(self.frame_1)
+        self.entrada02.place ( relx = 0.60, rely = 0.10, relwidth = 0.1, relheight = 0.1 )
 
-
+        #campo valor inteiro 03
+        self.entrada03 = Entry(self.frame_1)
+        self.entrada03.place ( relx = 0.80, rely = 0.10, relwidth = 0.1, relheight = 0.1 )
+    #Definir  funcções do back
+    def fx01(self) : 
+        
 
 
 
