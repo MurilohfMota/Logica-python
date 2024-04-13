06d83b3d7330f6a255d9b71923dfc92f6fe04594
Murilo Mota <murilohfmota@gmail.com>
2024-04-13 20:40:40 -0300

    Finalizado questão 01

--- exercicio 04.py
+++ exercicio 04.py
@@ -1,7 +1,9 @@
 from tkinter import *
 
 root = Tk()
-class App():
+label_var = StringVar()
+label_var2 = StringVar()
+class App() :
     #Definir telas e frames do front
     def __init__(self):
         self.root = root
@@ -14,7 +16,7 @@ class App():
         root.mainloop()
         
     def tela(self) :
-        self.root.title( "Exercicio 04" ) 
+        self.root.title( "Lab 13/04 questão 01" ) 
         self.root.configure ( background="#184e77")
         self.root.geometry ( "1250x720" )
         self.root.resizable (True, True)
@@ -31,19 +33,17 @@ class App():
     #construtor de botões
     def Construtor_botões(self):
         #botâo de executar
-        self.bt_executar = Button(self.frame_1, text = 'Executar', bd = 2 , bg = '#184e77', fg = '#d9ed92', font = ('firacode', 12))
+        self.bt_executar = Button(self.frame_1, text = 'Executar', bd = 2 , bg = '#184e77', fg = '#d9ed92', font = ('firacode', 12), command = self.fx01)
         self.bt_executar.place ( relx = 0.9, rely = 0.85, relwidth = 0.1, relheight = 0.15)
-        #botão proximo
-        self.bt_proximo = Button(self.root, text = 'Proximo', bd = 2 , bg = '#52b69a', fg = '#184e77', font = ('firacode', 12))
-        self.bt_proximo.place ( relx = 0.89, rely = 0.87, relwidth = 0.1, relheight = 0.07)
+
         #botão sair
-        self.bt_proximo = Button(self.root, text = 'Sair', bd = 2 , bg = '#99d98c', fg = '#184e77', font = ('firacode', 12))
+        self.bt_proximo = Button(self.root, text = 'Sair', bd = 2 , bg = '#99d98c', fg = '#184e77', font = ('firacode', 12), command = root.destroy)
         self.bt_proximo.place ( relx = 0.02, rely = 0.87, relwidth = 0.1, relheight = 0.07)
     
     def Caixa_texto(self) :
         #texto de instrução da primeira tela
-        self.texto = Label(self.frame_1, text = "Informe dois números inteiros e um número real.", background = '#d9ed92', fg = '#184e77', font = ('firacode', 12))
-        self.texto.place ( relx = 0.0, rely = 0.010, relwidth = 0.3, relheight = 0.1 )
+        self.texto = Label(self.frame_1, text = f"Informe dois números inteiros e um número real.\n Por favor preencha todos os Campos", background = '#d9ed92', fg = '#184e77', font = ('firacode', 12))
+        self.texto.place ( relx = 0.0, rely = 0.010, relwidth = 0.3, relheight = 0.17 )
         #texto numero 1
         self.texto01 = Label(self.frame_1, text = "Primeiro inteiro", background = '#d9ed92', fg = '#184e77', font = ('firacode', 12))
         self.texto01.place ( relx = 0.3, rely = 0.010, relwidth = 0.3, relheight = 0.1 )
@@ -55,7 +55,7 @@ class App():
         self.texto03.place ( relx = 0.7, rely = 0.010, relwidth = 0.3, relheight = 0.1,  )
 
         #caixa de texto do resultado no frame_2
-        self.texto_resultado = Label(self.frame_2, text = "", background = '#d9ed92')
+        self.texto_resultado = Label(self.frame_2, text = "", background = '#d9ed92', textvariable= label_var)
         self.texto_resultado.place ( relx = 0.0, rely = 0.010, relwidth = 0.9, relheight = 0.9 )
     def Widgets(self) : 
         #criando campos de entrada de texto do usuario
@@ -73,8 +73,21 @@ class App():
     #Definir  funcções do back
     def fx01(self) : 
         
+        if (self.entrada01 != NONE and self.entrada02 != NONE and self.entrada03 != NONE) :
+            self.int01 = int (self.entrada01.get())
+            self.int02 = int (self.entrada02.get())
+            self.float01 = float (self.entrada03.get())
+
+            #questão 1) a)
+            self.resultado_a = ((self.int01 * 2) * (self.int02/2))
+
+            #questão 1) b)
+            self.resultado_b = ((self.int01 **3 )+ self.float01 )
 
 
+            label_var.set(f'O produto do dobro do primeiro pela metade do segundo é {self.resultado_a}\nA soma do tripo do primeiro com o terceiro é {self.resultado_b}')
+        else :
+            label_var.set(f'Por favor, preencha todos os campos.')
 
 
 
